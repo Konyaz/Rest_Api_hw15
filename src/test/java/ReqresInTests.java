eef6acfcabe88163cfd56f6a8dd4b2bc128610a2
Konyaz <80565760+Konyaz@users.noreply.github.com>
2021-05-18 01:23:00 +0900

    add

--- src/test/java/ReqresInTests.java
+++ src/test/java/ReqresInTests.java
@@ -1,10 +1,12 @@
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 public class ReqresInTests {
 
 
@@ -14,7 +16,7 @@ public class ReqresInTests {
     }
 
     @Test
-    void successUsersListTest() {
+    void successListRESOURCETest() {
         given()
                 .when()
                 .get("/api/unknown")
@@ -22,6 +24,95 @@ public class ReqresInTests {
                 .statusCode(200)
                 .body("page", is(1))
                 .body("total_pages", is(2))
+                .body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
+    }
+    @Test
+    public void successfulUsersLListTest() {
+        given().when()
+                .get("/api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("page", is(2))
+                .body("per_page", is(6))
+                .body("total", is(12))
+                .body("total_pages", is(2))
+                .body("data", not(nullValue()))
+                .body("support.url", is("https://reqres.in/#support-heading"))
+                .body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
+    }
+
+    @Test
+    void successRegisterTest() {
+        given()
+                .contentType(JSON)
+                .body("{ \"email\": \"eve.holt@reqres.in\", " +
+                        "\"password\": \"cityslicka\" }")
+                .when()
+                .post("/api/register")
+                .then()
+                .statusCode(200)
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    void successDELAYEDRESPONSETest() {
+        given()
+                .contentType(JSON)
+                .when()
+                .get("api/users?page=2")
+                .then()
+                .statusCode(200)
+                .body("page", is(2))
+                .body("total", is(12))
+                .body("total_pages", is(2));
+
+    }
+
+    @Test
+    public void unsuccessfulLoginTest() {
+        given().contentType(ContentType.JSON)
+                .body("{ \"email\": \"eve.holt@reqres.in\"}")
+                .when()
+                .post("/api/login")
+                .then()
+                .statusCode(400)
+                .body("error", is("Missing password"));
+    }
+
+    @Test
+    public void successfulDeleteUserTest() {
+        given().contentType(ContentType.JSON)
+                .when()
+                .delete("/api/users/2")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    public void successfulRegistrationTest() {
+        given().contentType(ContentType.JSON)
+                .body("{ \"email\": \"eve.holt@reqres.in\", " +
+                        "\"password\": \"pistol\" }")
+                .when()
+                .post("/api/register")
+                .then()
+                .statusCode(200)
+                .body("id", is(4))
+                .body("token", is("QpwL5tke4Pnpja7X4"));
+    }
+
+    @Test
+    public void successfulSingleUserTest() {
+        given().when()
+                .get("/api/users/2")
+                .then()
+                .statusCode(200)
+                .body("data.id", is(2))
+                .body("data.email", is("janet.weaver@reqres.in"))
+                .body("data.first_name", is("Janet"))
+                .body("data.last_name", is("Weaver"))
+                .body("data.avatar", is("https://reqres.in/img/faces/2-image.jpg"))
+                .body("support.url", is("https://reqres.in/#support-heading"))
                 .body("support.text", is("To keep ReqRes free, contributions towards server costs are appreciated!"));
     }
 }
